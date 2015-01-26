9485483d882357669a2335a1214621ad08754bc7
Alex Pate <proto.merkabah@gmail.com>
2015-01-26 15:37:38 -0500

    Header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,18 @@
 // orgs[], starred[], repos[], user{}
 
 
+
+
+
+
+
+
+
+
+// TEMPLATES
+
+// use mustache symbols vs erb.
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
+var header_avatar = $('header-avatar');
+
